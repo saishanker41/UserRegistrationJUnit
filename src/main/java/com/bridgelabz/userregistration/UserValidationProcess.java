f88fdbd402dc234e89fb011a00f235a295ee1b47
saishanker41 <69297472+saishanker41@users.noreply.github.com>
2021-03-28 00:58:20 +0530

    [sai] Added user first name

--- src/main/java/com/bridgelabz/userregistration/UserValidationProcess.java
+++ src/main/java/com/bridgelabz/userregistration/UserValidationProcess.java
@@ -1,5 +1,12 @@
 package com.bridgelabz.userregistration;
 
 public class UserValidationProcess {
+    public boolean validateFirstName(String name) {
+        String pattern = "^([A-Z]{1})([a-z]{2,})$";
+        if (name.matches(pattern))
+            return true;
+        else
+            return false;
+    }
 
 }
